ee0bd77b687e3a5b49ef78b88f9f55883229b66a
gufanfan <stefan.gufan@gmail.com>
2021-06-07 17:57:45 +0800

    feat: 扩展修改

--- lib-basic/src/main/java/com/frlib/basic/binding/BindingAdapters.kt
+++ lib-basic/src/main/java/com/frlib/basic/binding/BindingAdapters.kt
@@ -8,6 +8,7 @@ import com.frlib.basic.image.displayImage
 import com.frlib.basic.image.displayRadiusImage
 import com.frlib.utils.ext.dp2px
 import com.frlib.utils.ext.drawable
+import com.frlib.utils.ext.length
 
 /**
  * @author Fanfan Gu <a href="mailto:stefan.gufan@gmail.com">Contact me.</a>
@@ -21,16 +22,22 @@ fun bindImage(imageView: ImageView, imageRes: Int) {
 }
 
 @BindingAdapter(value = ["imageUrl", "placeholder"])
-fun bindImage(imageView: ImageView, imageUrl: String, placeholder: Drawable) {
-    imageView.displayImage(imageUrl, placeholder = placeholder)
+fun bindImage(imageView: ImageView, imageUrl: String?, placeholder: Drawable) {
+    if (imageUrl.length() > 0) {
+        imageView.displayImage(imageUrl!!, placeholder = placeholder)
+    }
 }
 
 @BindingAdapter(value = ["imageUrl", "radius", "placeholder"])
-fun bindRadiusImage(imageView: ImageView, imageUrl: String, radius: Int, placeholder: Drawable) {
-    imageView.displayRadiusImage(imageUrl, imageView.context.dp2px(radius.toFloat()), placeholder = placeholder)
+fun bindRadiusImage(imageView: ImageView, imageUrl: String?, radius: Int, placeholder: Drawable) {
+    if (imageUrl.length() > 0) {
+        imageView.displayRadiusImage(imageUrl!!, imageView.context.dp2px(radius.toFloat()), placeholder = placeholder)
+    }
 }
 
 @BindingAdapter(value = ["circleUrl", "placeholder"])
-fun bindCircleImage(imageView: ImageView, circleUrl: String, placeholder: Drawable) {
-    imageView.displayCircleImage(circleUrl, placeholder = placeholder)
+fun bindCircleImage(imageView: ImageView, circleUrl: String?, placeholder: Drawable) {
+    if (circleUrl.length() > 0) {
+        imageView.displayCircleImage(circleUrl!!, placeholder = placeholder)
+    }
 }
--- lib-utils/src/main/java/com/frlib/utils/ext/IntExt.kt
+++ lib-utils/src/main/java/com/frlib/utils/ext/IntExt.kt
@@ -1,7 +1,19 @@
 package com.frlib.utils.ext
 
+import java.math.BigDecimal
+
 /**
  * @author Fanfan Gu <a href="mailto:stefan.gufan@gmail.com">Contact me.</a>
  * @date 08/04/2021 19:54
  * @desc 整数扩展
  */
+val RMB_DECIMAL by lazy {
+    BigDecimal(100)
+}
+
+/**
+ * 价格换算, 分转元
+ */
+fun Int.cent2Yuan(): String {
+    return BigDecimal(this).divide(RMB_DECIMAL).toString()
+}
